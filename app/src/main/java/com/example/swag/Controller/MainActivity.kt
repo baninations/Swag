fb9398ac7b62e84af0ce15d89eba9b18521abba1
Alban <baninations@gmail.com>
2020-06-17 19:43:23 +0200

    After DataServices and GUI

--- app/src/main/java/com/example/swag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/swag/Controller/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.swag.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.swag.Model.Category
+import com.example.swag.Model.Services.DataService
 import com.example.swag.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        adapter = ArrayAdapter(this,
+               android.R.layout.simple_list_item_1,
+                DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
