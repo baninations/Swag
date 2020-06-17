4011cd5ffc00e758f9bc84ade6a8238a38aeb29a
Alban <baninations@gmail.com>
2020-06-17 20:58:14 +0200

    Lesson 3, Completed a Custom list adapter

--- app/src/main/java/com/example/swag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/swag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.swag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.example.swag.Adapters.CategoryAdapter
 import com.example.swag.Model.Category
 import com.example.swag.Model.Services.DataService
 import com.example.swag.R
@@ -10,16 +11,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        adapter = ArrayAdapter(this,
-               android.R.layout.simple_list_item_1,
-                DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
     }
 }
